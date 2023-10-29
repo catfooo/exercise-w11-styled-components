617b933a17c78423324b93bb0f73d26b1eb02acb
So Youn Choi <manghatuhyo@gmail.com>
2023-10-29 22:12:19 +0100

    working example 3

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: #bf4f74;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+export const App = () => {
+  return (
+    <div>
+      <Button>Normal Button</Button>
+      <TomatoButton>Tomato Button</TomatoButton>
+    </div>
+  );
+};
