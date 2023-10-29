363359e2194f98d17e8c2d04bd0c5318bf3b572d
So Youn Choi <manghatuhyo@gmail.com>
2023-10-29 22:04:54 +0100

    working example 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
-interface ButtonProps {
+type ButtonProps = {
   $primary?: boolean;
-}
+};
 
 const Button = styled.button<ButtonProps>`
-  /* Adapt the colors based on primary prop */
   background: ${props => props.$primary ? "blue" : "white"};
   color: ${props => props.$primary ? "white" : "blue"};
 `;
